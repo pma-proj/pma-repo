a14628355b1843bc7ad19400525a269d858c804f
pma-proj <jason.tresfield@hotmail.fr>
2015-01-12 17:40:36 +0100

    [Projets] Ajout de la session ProjectsList.

---
+++ PMAServer/PMAWeb/Tools/ConstantsManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMAWeb.Tools
+{
+    public class ConstantsManager
+    {
+        #region Sessions Names
+        public const string ProjectsListSession = "ProjectsList";
+        #endregion
+    }
+}
---
+++ PMAServer/PMAWeb/Tools/SessionManager.cs
@@ -0,0 +1,23 @@
+﻿using PMAWeb.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMAWeb.Tools
+{
+    public class SessionManager
+    {
+        public static List<ProjectModel> ProjectsList
+        {
+            get
+            {
+                return HttpContext.Current.Session[ConstantsManager.ProjectsListSession] as List<ProjectModel>;
+            }
+            set
+            {
+                HttpContext.Current.Session[ConstantsManager.ProjectsListSession] = value;
+            }
+        }
+    }
+}
